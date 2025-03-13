247c9f9f484633fe003c9ddda5018b2b21ad9140
pu0238 <pu0238@protonmail.com>
2025-03-13 17:16:31 +0000

    Added error validation and dynamic size calculation

--- src/icp_workshops_backend/src/lib.rs
+++ src/icp_workshops_backend/src/lib.rs
@@ -15,30 +15,28 @@ pub struct Response {
 }
 
 #[ic_cdk::update]
-async fn translate() -> String {
+async fn translate(text: String) -> Result<String, String> {
     let token = "";
     let arg = CanisterHttpRequestArgument {
         url: "https://api-inference.huggingface.co/models/google-t5/t5-base".to_string(),
         max_response_bytes: None,
-        method: HttpMethod::GET,
+        method: HttpMethod::POST,
         headers: vec![
             HttpHeader {
                 name: "Authorization".to_string(),
                 value: format!("Bearer {}", token).to_string(),
             }
         ],
-        body: Some(r#"{"inputs": "What's up?"}"#.into()),
+        body: Some(format!(r#"{{"inputs": "{}"}}"#, text).into()),
         transform: None,
     };
     let res = http_request(
         arg,
-        100_000_000_000
-    ).await.unwrap();
+        (1_603_112_800 + text.len() * 400).try_into().unwrap()
+    ).await.map_err(|error| format!("Error while querying data. Status: {:?}, Error: {}", error.0, error.1))?;
 
-    println!("{:?}", res);
-    println!("{:?}", String::from_utf8(res.0.body.clone()));
+    println!("123 {:?}", res);
 
-    let formated_res: (Response,) = serde_json::from_slice(&res.0.body).unwrap();
-
-    String::new()
+    let formated_res: (Response,) = serde_json::from_slice(&res.0.body).map_err(|error| format!("Error while parsing data. Error: {}", error))?;
+    Ok(formated_res.0.translation_text)
 }
